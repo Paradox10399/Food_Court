43ac359ce8d1ff9c54681ae6681a952dcbcd1e5d
Paradox10399 <m.a.hameed10399@gmail.com>
2024-01-13 16:47:27 +0530

    Created Folders Structure

---
+++ src/Pages/index.js
@@ -0,0 +1,11 @@
+import CategoryPage from "./CategoryPage/CategoryPage";
+import HomePage from "./HomePage/HomePage";
+import ErrorPage from "./ErrorPage/ErrorPage";
+import MealDetail from "./MealDetail/MealDetailPage";
+
+export{
+    CategoryPage,
+    HomePage,
+    ErrorPage,
+    MealDetail
+}
