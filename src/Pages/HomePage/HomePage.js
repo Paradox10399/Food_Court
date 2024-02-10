9ab31110986672bbd638b6b0b824f15d32b13537
Paradox10399 <m.a.hameed10399@gmail.com>
2024-02-10 21:21:16 +0530

    Project Completed

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -2,14 +2,19 @@ import React from 'react'
 import { useMealContext } from '../../Context/mealContext'
 import Loader from '../../components/Loader/Loader'
 import CategoryList from '../..//components/Category/CategoryList'
+import NotFound from '../../components/NotFound/NotFound'
+import MealList from '../../components/Meal/MealList'
 
 function HomePage() {
-  const { categories, meals, categoryLoading, mealsLoading} = useMealContext();
+  const { categories, meals, categoryLoading, mealsLoading } = useMealContext();
   console.log(categories, meals, categoryLoading, mealsLoading);
   return (
     <main className='container'>
       {
-        (categoryLoading) ? <Loader/> : <CategoryList categories={categories} /> 
+        (mealsLoading) ? <Loader /> : (meals === null) ? <NotFound /> : (meals?.length) ? <MealList meals={meals} /> : ''
+      }
+      {
+        (categoryLoading) ? <Loader /> : <CategoryList categories={categories} />
       }
     </main>
   )
--- src/components/Header/SearchForm.js
+++ src/components/Header/SearchForm.js
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useMealContext } from '../../Context/mealContext';
+import { useNavigate } from 'react-router-dom';
+import { startFetchMealBySearch } from '../../actions/mealActions';
 
 function SearchForm() {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("")
+  const[errorMsg, setErrorMsg] = useState("")
+  const {dispatch, meals} = useMealContext()
+
+const handleSearchTerm = (e) => {
+  e.preventDefault();
+  if ((e.target.value.replace(/[^\w\s]/gi,'')).length !== 0){
+    setSearchTerm(e.target.value)
+    setErrorMsg("")
+  }else{
+    setErrorMsg("Invalid search term ...")
+  }
+  }
+  const handleSearchResult = (e) => {
+    e.preventDefault();
+    startFetchMealBySearch(dispatch, searchTerm)
+    navigate("/");
+  }
+
   return (
-    <form className='d-flex align-items-center'>
+    <form className='d-flex align-items-center' onSubmit={(e) => handleSearchResult(e)}>
       <input
+        onChange={(e) => handleSearchTerm(e)}
         type='text'
         className='form-control'
         name="search"
         id="search"
         placeholder="Search Recipes here..."
       />
-      <button className='btn btn-dark'>
+      <button type='button' className='btn btn-dark'>
         <i className='fa-solid fa-search'></i>
       </button>
     </form>
   )
+
 }
+
+
 
 export default SearchForm
